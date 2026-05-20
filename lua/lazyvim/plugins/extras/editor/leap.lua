c7bf49a6e3ae865c8a02325b3dbeb6a302487e75
thyttan <97237430+thyttan@users.noreply.github.com>
2026-05-21 00:30:20 +0200

    Fix leap.nvim extra not working

--- lua/lazyvim/plugins/extras/editor/leap.lua
+++ lua/lazyvim/plugins/extras/editor/leap.lua
@@ -32,6 +32,45 @@ return {
       leap.add_default_mappings(true)
       vim.keymap.del({ "x", "o" }, "x")
       vim.keymap.del({ "x", "o" }, "X")
+
+      -- leap.nvim broke for me at some point. This make two char search work again. From https://github.com/LazyVim/LazyVim/issues/2379#issuecomment-1898491969
+      vim.keymap.set("n", "s", function()
+        require("leap").leap({ target_windows = { vim.api.nvim_get_current_win() } })
+      end)
+
+      -- ft broke at some point for me. This makes it work again. From https://codeberg.org/andyg/leap.nvim#search-and-motions.
+      do
+        local function ft(key_specific_args)
+          require("leap").leap(vim.tbl_deep_extend("keep", key_specific_args, {
+            inputlen = 1,
+            inclusive = true,
+            opts = {
+              -- Force autojump.
+              labels = "",
+              -- Match the modes where you don't need labels (`:h mode()`).
+              safe_labels = vim.fn.mode(1):match("o") and "" or nil,
+            },
+          }))
+        end
+
+        -- A helper function making it easier to set "clever-f" behavior
+        -- (using f/F or t/T instead of ;/, - see the plugin clever-f.vim).
+        local clever = require("leap.user").with_traversal_keys
+        local clever_f, clever_t = clever("f", "F"), clever("t", "T")
+
+        vim.keymap.set({ "n", "x", "o" }, "f", function()
+          ft({ opts = clever_f })
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "F", function()
+          ft({ backward = true, opts = clever_f })
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "t", function()
+          ft({ offset = -1, opts = clever_t })
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "T", function()
+          ft({ backward = true, offset = 1, opts = clever_t })
+        end)
+      end
     end,
   },
 
